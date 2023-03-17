a6ebc51a91820db59c7fb6cbb8ad969917a734ad
Aleksandr_Shcherbakov <alexcht@mail.ru>
2023-03-17 23:20:46 +0300

    Added method 'detectRuEn()'

--- functions.py
+++ functions.py
@@ -9,3 +9,23 @@ def validateNumInput(str1, str2, str3='', n=0):
                 return num
             else:
                 print(str3)
+
+
+def validateYesNo():
+    while True:
+        decision = input()
+        if decision.lower() == "да":
+            return True
+        elif decision.lower() == "нет":
+            return False
+        else:
+            print("Введите \"да\", или \"нет\": ")
+
+
+def detectRuEn(c):
+    if (c >= "а" and c <= "я") or (c >= "А" and c <= "Я"):
+        return 'ru'
+    elif (c >= "a" and c <= "z") or (c >= "A" and c <= "Z"):
+        return 'en'
+    else:
+        return ''
